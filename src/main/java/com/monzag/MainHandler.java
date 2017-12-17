5bf4c70a96b1740fec96d102ec0099ab8b47fe16
monzag <zaganiacz.m@gmail.com>
2017-12-17 23:49:17 +0100

    Add MainHandler

---
+++ src/main/java/com/monzag/MainHandler.java
@@ -0,0 +1,13 @@
+package com.monzag;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class MainHandler implements HttpHandler {
+
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+    }
+}
