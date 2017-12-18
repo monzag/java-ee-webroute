754ba91f77a0f95f0a7495c2c2e70fa25f88b251
monzag <zaganiacz.m@gmail.com>
2017-12-18 02:20:54 +0100

    Add MainHandler

--- src/main/java/com/monzag/MainHandler.java
+++ src/main/java/com/monzag/MainHandler.java
@@ -4,10 +4,31 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 public class MainHandler implements HttpHandler {
 
     public void handle(HttpExchange httpExchange) throws IOException {
 
+        String path = httpExchange.getRequestURI().getPath();
+
+        try {
+            Class controller = Controller.class;
+
+            for (Method method: controller.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(WebRoute.class)) {
+                    Annotation annotation = method.getAnnotation(WebRoute.class);
+                    WebRoute route = (WebRoute) annotation;
+
+                    if (path.equals(route.value())) {
+                        method.invoke(controller.newInstance(), httpExchange);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
